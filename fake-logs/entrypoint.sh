6220ccab1c9f8fa66d6afa5bdb3075ab57424bc3
ductnn <trannhuduc531998@gmail.com>
2022-12-08 16:59:47 +0700

    update: color logs

--- fake-logs/entrypoint.sh
+++ fake-logs/entrypoint.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
+
+# color
+red="\033[1;31m"
+green="\033[1;32m"
+yellow="\033[0;33m"
+blue="\033[0;34m"
+
+bwhite="\033[1;37m"
+
+no_color="\033[0m"
+
 n=-1
 c=0
+
 if [ -n "$3" ]
 then
    n=$3
@@ -13,13 +25,13 @@ do
    I=$(shuf -i 1-4 -n 1)
    D=`date -Iseconds`
    case "$I" in
-      "1") echo "$D ERROR An error is usually an exception that has been caught and not handled."
+      "1") echo -e "${bwhite}$D${no_color} ${red}ERROR${no_color} An error is usually an exception that has been caught and not handled."
       ;;
-      "2") echo "$D INFO This is less important than debug log and is often used to provide context in the current task."
+      "2") echo -e "${bwhite}$D${no_color} ${green}INFO${no_color} This is less important than debug log and is often used to provide context in the current task."
       ;;
-      "3") echo "$D WARN A warning that should be ignored is usually at this level and should be actionable."
+      "3") echo -e "${bwhite}$D${no_color} ${yellow}WARN${no_color} A warning that should be ignored is usually at this level and should be actionable."
       ;;
-      "4") echo "$D DEBUG This is a debug log that shows a log that can be ignored."
+      "4") echo -e "${bwhite}$D${no_color} ${blue}DEBUG${no_color} This is a debug log that shows a log that can be ignored."
       ;;
    esac
    c=$(( c+1 ))
